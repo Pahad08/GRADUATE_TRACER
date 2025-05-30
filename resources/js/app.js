55edcf51a74cf7f679c8ed53fe377d92e08e6ad9
Fahad Bagundang <mastahpahad@gmail.com>
2025-05-30 10:06:08 +0800

    import fontawesome

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,4 @@
 import "@wotz/livewire-sortablejs";
+import "./fontawesome/fontawesome";
 import ApexCharts from "apexcharts";
 window.ApexCharts = ApexCharts;
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="shortcut icon" href="{{ asset("images/logo.png") }}" type="image/x-icon">
-        @vite(["resources/css/app.css", "resources/js/app.js", "resources/js/fontawesome/fontawesome.js"])
+        @vite(["resources/css/app.css", "resources/js/app.js"])
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <title>{{ $title ?? "Graduate Tracer" }}</title>
